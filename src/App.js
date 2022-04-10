17ba8e23aa3e9a48e286fa0e104cffaebe2a2a01
Talex210 <tal.89@mail.ru>
2022-04-10 14:56:56 +0300

    Получилось захостить страничку. Ура!!

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ function App() {
     return (
         <div className="App">
             Хостим React приложение
+            <button>fgsfhggfhfgh</button>
+            <button>fdghdghjhjghjhg</button>
+            <button>fdhdghjghjgh</button>
+            <button>sdsfvf</button>
+            <button>fgngdh</button>
         </div>
     );
 }
