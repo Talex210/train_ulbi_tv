2fb3f4cc3bf0d612da62735df64ca1d5166a6582
Talex210 <tal.89@mail.ru>
2022-04-12 00:18:46 +0300

    Работа с состоянием, useState. Управляемый input - управляемый компонент, двухсторонее связывание

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function App() {
+    const [likes, setLikes] = useState(5)
+    const [value, setValue] = useState('Text in input')
+
+    function increment() {
+        setLikes(likes + 1)
+    }
+
+    function decrement() {
+        setLikes(likes - 1)
+    }
+
     return (
         <div className="App">
-            Хостим React приложение
-            <button>fgsfhggfhfgh</button>
-            <button>fdghdghjhjghjhg</button>
-            <button>fdhdghjghjgh</button>
-            <button>sdsfvf</button>
-            <button>fgngdh</button>
+            <h1>{likes}</h1>
+            <h1>{value}</h1>
+            <input type='text' value={value} onChange={event => setValue(event.target.value)}/>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
         </div>
     );
 }
