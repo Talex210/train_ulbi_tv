bbfff2282dbcbd33870d7891da922c39370e21a6
Talex210 <tal.89@mail.ru>
2022-05-17 17:20:32 +0300

    Working with the list. Converting an array of objects into an array of React elements.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 // import ClassCounter from "./Components/ClassCounter";
 // import Counter from "./Components/Counter";
 import './styles/App.css'
-import PostItem from "./Components/PostItem";
+import PostList from "./Components/PostList";
 
 function App() {
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'Java Script', body: 'Description'},
+        {id: 2, title: 'Java Script 2', body: 'Description'},
+        {id: 3, title: 'Java Script 3', body: 'Description'},
+        {id: 4, title: 'Java Script 4', body: 'Description'},
+        {id: 5, title: 'Java Script 5', body: 'Description'}
+    ]);
+    const [posts2, setPosts2] = useState([
+        {id: 1, title: 'Python', body: 'Description'},
+        {id: 2, title: 'Python 2', body: 'Description'},
+        {id: 3, title: 'Python 3', body: 'Description'},
+        {id: 4, title: 'Python 4', body: 'Description'},
+        {id: 5, title: 'Python 5', body: 'Description'}
+    ]);
     return (
         <div className='App'>
             {/*<Counter/>*/}
             {/*<ClassCounter/>*/}
-            <PostItem post={{id: 1, title: 'Java Script', body: 'Description'}}/>
-            <PostItem post={{id: 2, title: 'Java Script', body: 'Description'}}/>
-            <PostItem post={{id: 3, title: 'Java Script', body: 'Description'}}/>
-            <PostItem post={{id: 4, title: 'Java Script', body: 'Description'}}/>
-            <PostItem post={{id: 5, title: 'Java Script', body: 'Description'}}/>
-            <PostItem post={{id: 6, title: 'Java Script', body: 'Description'}}/>
+            <PostList posts={posts} title={'Посты про JS'}/>
+            <PostList posts={posts2} title={'Посты про Python'}/>
         </div>
     );
 }
